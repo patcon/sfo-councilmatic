e71d4482f8ac52ef17cb64e42f591ee3b262b442
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-02-21 03:00:48 -0800

    Preparing for heroku deploy.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn councilmatic.wsgi --log-file -
--- councilmatic/settings.py
+++ councilmatic/settings.py
@@ -107,3 +107,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

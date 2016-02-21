dbc961d8e4ac63422deba48a1d940f4fac66854b
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-02-21 03:23:37 -0800

    Fixed database settings.

--- councilmatic/settings_deployment.py
+++ councilmatic/settings_deployment.py
@@ -13,6 +13,16 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sfo_councilmatic',
+        'USER': '',
+        'PASSWORD': '',
+    }
+}
+
+
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
